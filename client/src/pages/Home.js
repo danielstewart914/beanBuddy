d86f4c2772a8a38dc7698aa02156e6d1d95311d5
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-11-02 18:37:48 -0700

    Started Boilerplate

---
+++ client/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+      <div className='flex-row justify-center'>
+        <div
+          className='col-12 col-md-10 mb-3 p-3'
+          style={{ border: '1px dotted #1a1a1a' }}
+        >
+          Content
+        </div>
+        <div className='col-12 col-md-8 mb-3'>
+          {}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
