c05ea33e4235d800420fd25dcbdfd898969bab93
Zachary Warnes <zacharywarnes@gmail.com>
2022-11-07 20:48:43 -0800

    adding query pointers to coffee page

--- client/src/pages/Coffee.js
+++ client/src/pages/Coffee.js
@@ -1,19 +1,40 @@
 import React from 'react';
 
-import {COFFEE_SEARCH} from '../utils';
+import {COFFEE_SEARCH} from '../utils/queries';
 import {useParams} from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 
 const Coffee = () => {
-    const {id: coffeeId} = useParams();
-    const {loading} = useQuery(COFFEE_SEARCH, {
-        variables: {coffeeId}
+    const { coffeeId } = useParams();
+
+    const { loading, data } = useQuery(COFFEE_SEARCH, {
+        variables: {coffeeId: coffeeId},
     });
+
+    const coffee = data?.coffee || {};
+    
+    if (loading) {
+        return <div>Loading... </div>;
+    } 
     
     return (
         <div className='flex-row justify-center mb-3'>
-            <h2 className='col-12 col-md-10 bg-dark text-light p-3 mb-5'> Coffee Name </h2>
+            <h2 className='col-12 col-md-10 bg-dark text-light p-3 mb-5'> Coffee Name: {coffee.name} </h2>
+            <div>
+                <img>Placeholder</img>
+            </div>
+            <div>
+                <li>
+                    <ul>{coffee.brand}</ul>
+                    <ul>{coffee.roast}</ul>
+                    <ul>{coffee.beanType}</ul>
+                    <ul>{coffee.origin}</ul>
+                </li>
+            </div>
+
+
+
         </div>
 
     )
