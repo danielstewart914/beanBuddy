c0e12d6651243185bafd4c52fb9888b77458faac
azm89 <ashtonmoore89@gmail.com>
2022-11-09 19:19:57 -0800

    updated AddReview page and added ADD_REVIEW mutation to mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,20 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_REVIEW = gql`
+  mutation addReview($coffee: String!, $coffeeRating: Int!, $grind: String!, $brewMethod: String!, $reviewText: String!, $image: String!, $flavorProfile: String!) {
+    addReview(coffee: $coffee, coffeeRating: $coffeeRating, grind: $grind, brewMethod: $brewMethod, reviewText: $reviewText, image: $image, flavorProfile: $flavorProfile) {
+      _id
+      coffee
+      coffeeRating
+      grind
+      brewMethod
+      reviewText
+      image
+      flavorProfile
+      createdAt
+      username
+    }
+  }
+`;
