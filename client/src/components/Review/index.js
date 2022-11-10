4126481bb143ee0db40abead1b244090419e9e21
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-11-09 21:07:52 -0800

    Created StarRatingInput

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -16,3 +16,5 @@ const Review = ({
     )
 
 }
+
+export default Review;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import StarRating from '../components/StarRating';
+import StarRatingInput from '../components/StarRatingInput';
 
 const Home = () => {
+  const [ rating, setRating ] = useState( 1 );
   return (
     <main>
       <div className='flex-row justify-center'>
@@ -8,10 +11,10 @@ const Home = () => {
           className='col-12 col-md-10 mb-3 p-3'
           style={{ border: '1px dotted #1a1a1a' }}
         >
-          Content
+          <StarRatingInput rating={rating} setRating={setRating} />
         </div>
         <div className='col-12 col-md-8 mb-3'>
-          {}
+          <StarRating rating={ 4 } />
         </div>
       </div>
     </main>
