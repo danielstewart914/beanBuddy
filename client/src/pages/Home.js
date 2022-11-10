a500ee210b671435e5e0ef71d38cd407cbb157ab
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-11-09 21:10:19 -0800

    Removed Test code from homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
-import StarRating from '../components/StarRating';
-import StarRatingInput from '../components/StarRatingInput';
+import React from 'react';
 
 const Home = () => {
-  const [ rating, setRating ] = useState( 1 );
   return (
     <main>
       <div className='flex-row justify-center'>
@@ -11,10 +8,9 @@ const Home = () => {
           className='col-12 col-md-10 mb-3 p-3'
           style={{ border: '1px dotted #1a1a1a' }}
         >
-          <StarRatingInput rating={rating} setRating={setRating} />
         </div>
         <div className='col-12 col-md-8 mb-3'>
-          <StarRating rating={ 4 } />
+          {}
         </div>
       </div>
     </main>
