c256201a0c1ab31404dca9779a5e2b27f800d2f6
Zachary Warnes <zacharywarnes@gmail.com>
2022-11-12 09:29:49 -0800

    changing NavBar index

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -7,16 +7,13 @@ import styles from './NavBar.module.css';
 const NavBar = () => {
 
   return (
-    <div className= {styles.NavBar}>
-    <Link className='Link-Button' to='/'> Home </Link>
-    <Link className='Link-Button' to='/add-review'> Add Review </Link>
-
-
     
     <div className={ styles.NavBar }>
+    <Link className='Link-Button' to='/'> Home </Link>
+    <Link className='Link-Button' to='/add-review'> Add Review </Link>
         <SearchBar />
     </div>
-    </div>
+    
   )
 }
 
